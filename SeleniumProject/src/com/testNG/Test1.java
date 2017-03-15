e842f56a29526f562bd3b2bf2708ebbed41b7b6e
MAGUSHAI <MAGUSHAI@ADMINRG-95TP26O>
2017-03-15 08:10:53 +0530

    Initial Checkin for Test1

---
+++ SeleniumProject/src/com/testNG/Test1.java
@@ -0,0 +1,19 @@
+package com.testNG;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void test2()
+	{
+		System.out.println("test2()");
+		System.out.println("Hi");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("test3()");
+	}
+}
